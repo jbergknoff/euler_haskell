9e534bc5edf85e3001b156334d0f5faba91c5f7a
jbergknoff <jonathan@bergknoff.com>
2014-03-15 22:16:08 +0000

    problem 10 ... kinda

---
+++ 10.hs
@@ -0,0 +1,49 @@
+{-
+
+https://projecteuler.net/problem=10
+
+	The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+	Find the sum of all the primes below two million.
+
+-}
+
+-- I tried the naive listing of primes as in problem 3, but it was way too slow.
+-- Instead, try a sieve?
+
+primes_below n = sieve [ 2 .. (n-1) ]
+
+--import Data.List
+--sieve (x:xs)
+--	| length xs == 0	= [ x ]
+--	| otherwise			= x : sieve (xs \\ [ 2*x, 3*x .. (last xs) ])
+
+sieve (x:xs)
+	| length xs == 0	= [ x ]
+	| otherwise			= x : sieve (filter (\y -> (mod y x) /= 0) xs)
+
+answer n = foldr (+) 0 (primes_below n)
+
+{-
+
+sieve [2,3,4,5,6]
+= 2 : sieve ([3,4,5,6] \\ [4, 6]) = 2 : sieve ([3,5])
+= 2 : 3 : sieve([5] \\ []) = 2 : 3 : sieve([5])
+= 2 : 3 : 5
+
+-}
+
+{-
+
+the sieve is extremely slow, both with Data.List list difference (\\) and
+with filtering. It speeds up as it goes along, as expected (smaller list to
+sieve on each subsequence step), but still very slow.
+
+how does Mathematica do it so fast?
+
+	Total[Select[Table[Prime[k], {k, 5*10^5}], # < 2*10^6 &]]
+	142913828922
+
+takes no time at all
+
+-}
