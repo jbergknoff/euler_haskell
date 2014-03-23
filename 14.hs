4fb8a936c1838e152de5f4370560f31ef81eef34
jbergknoff <jonathan@bergknoff.com>
2014-03-23 19:34:24 +0000

    "better" problem 14, still took a long time

--- 14.hs
+++ 14.hs
@@ -18,23 +18,59 @@ https://projecteuler.net/problem=14
 
 -}
 
-iterator :: Int -> Int
+iterator :: Integer -> Integer
 iterator n
 	| mod n 2 == 0	= (quot n 2)
 	| otherwise		= 3 * n + 1
 
-collatz :: [Int] -> [Int]
+collatz :: [Integer] -> [Integer]
 collatz x
 	| last x == 1	= x
 	| otherwise		= collatz (x ++ [ iterator(last x) ])
 
-lengths n = [ length(collatz([k])) | k <- [ 1 .. n ] ]
+-- lengths n = [ length(collatz([k])) | k <- [ 1 .. n ] ]
 
-answer n = maximum (zip (lengths n) [ 1 .. n ])
+-- answer n = maximum (zip (lengths n) [ 1 .. n ])
+
 
 {-
 
 theoretically the above works, but it didn't give an answer after 10 minutes.
 back to the drawing board?
 
+-}
+
+
+{-
+
+-- http://www.haskell.org/haskellwiki/Memoization
+
+memoized_fib :: Int -> Integer
+memoized_fib = ((map fib [0 ..]) !!)
+	where fib 0 = 0
+			fib 1 = 1
+			fib n = memoized_fib (n-2) + memoized_fib (n-1)
+
+-}
+
+collatz2 :: [Integer] -> [Integer]
+collatz2 x
+	| last x < head x	= x
+	| otherwise			= collatz2 (x ++ [ iterator(last x) ])
+
+memoized_collatz_length = (map collatz_length [1 ..])
+	where
+		collatz_length :: Int -> Int
+		collatz_length 1 = 1
+		collatz_length k = length(collatz2([toInteger(k)])) - 1 + (memoized_collatz_length !! fromInteger(last(collatz2([toInteger(k)])) - 1))
+
+lengths n = zip (take n memoized_collatz_length) [ 1 .. n ]
+answer n = maximum (lengths n)
+
+{-
+
+*Main> answer 1000000
+(525,837799)
+
+this took about 40 minutes. jeez.
 -}
